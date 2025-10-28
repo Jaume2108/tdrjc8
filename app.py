2cdb95b457a7e47cb6bc819b8b4051c3111cb32f
Jaume2108 <jribo1@iesguissona.cat>
2025-10-28 14:27:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -538,5 +538,3 @@ if __name__ == "__main__":
     from os import environ
     port = int(environ.get("PORT", 5000))  # Railway define PORT automáticamente
     app.run(host="0.0.0.0", port=port, debug=False)
-
-
